2553949694c3ff02d821417881ac26c90c77a1f5
Aps <aps@Apss-MacBook-Air.local>
2020-12-22 21:17:45 +0530

    test

--- src/Footer.js
+++ src/Footer.js
@@ -45,15 +45,16 @@ import "./Footer.css"
 
 
 var style = {
-    backgroundColor: "#F8F8F8",
     borderTop: "1px solid #E7E7E7",
     textAlign: "center",
     padding: "20px",
     position: "fixed",
     left: "0",
     bottom: "0",
-    height: "65px",
+    height: "90px",
     width: "100%",
+    backgroundColor: 'black',
+    color: 'white',
     
 }
 
